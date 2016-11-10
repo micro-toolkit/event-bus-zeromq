050ec8400a1e854051dc0932cd107f50b1116006
Pedro Januário <prnjanuario@gmail.com>
2016-11-10 14:45:27 +0000

    added eslint support

--- test/example_test.js
+++ test/example_test.js
@@ -1,5 +1,5 @@
 describe('Example spec', function () {
   it('should pass', function () {
-    "abc".should.be.eql("abc");
-  });
-});
+    "abc".should.be.eql("abc")
+  })
+})
--- test/support/index.js
+++ test/support/index.js
@@ -1,4 +1,4 @@
-const chai = require('chai')
+var chai = require('chai')
 
 chai.config.includeStack = true
 
