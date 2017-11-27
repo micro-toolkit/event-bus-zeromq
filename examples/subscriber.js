33be78bdeabd7252d9511db7abbc24b3e81fc301
Pedro Januário <prnjanuario@gmail.com>
2017-11-27 11:43:37 +0000

    fixed subscriber example

--- examples/subscriber.js
+++ examples/subscriber.js
@@ -15,9 +15,11 @@ subscriber.on('/examples', function(data){
 
 function close() {
   subscriber.close()
+  process.exit()
 }
 
 // eslint-disable-next-line no-process-exit
 process.on('SIGINT', close)
+process.on('SIGTERM', close)
 
 subscriber.connect()
